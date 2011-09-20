4db30b4b8a251705516374eb36af27734d9e9206
Matthias Reisinger <matthias.reisinger@the-hub.net>
2011-09-20 09:41:31 +0200

    alpha2 commit

---
+++ test/slider3_files/blogsponsor.js
@@ -0,0 +1,42 @@
+document.write('<div id="bsap_1258449" class="bsarocks bsap_ef0b652d262a926ea219420f9f2cf058"></div>');
+document.write('<style>.bsap{float:left;position:fixed;z-index:1000;top:20px;right:20px;width:300px;overflow:hidden;}.bsa_it_ad{overflow:hidden;}</style>');
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+
+/*if (navigator.appName != 'Microsoft Internet Explorer'){
+
+document.write('<link href="http://cssglobe.com/ads/blogsponsor.css" rel="stylesheet" type="text/css" media="screen" />');
+document.write('<div id="ads">');
+
+Vertical1239598 = false;
+ShowAdHereBanner1239598 = false;
+RepeatAll1239598 = false;
+NoFollowAll1239598 = false;
+BannerStyles1239598 = new Array(
+    "",
+    "",
+    "",
+    ""
+);
+document.write(unescape("%3Cscript src='"+document.location.protocol+"//s3.buysellads.com/1239598/1239598.js?v="+Date.parse(new Date())+"' type='text/javascript'%3E%3C/script%3E"));
+
+Vertical1239599 = false;
+ShowAdHereBanner1239599 = true;
+RepeatAll1239599 = false;
+NoFollowAll1239599 = false;
+BannerStyles1239599 = new Array(
+	"",
+	"",
+	"",
+	""
+);
+document.write(unescape("%3Cscript src='"+document.location.protocol+"//s3.buysellads.com/1239599/1239599.js?v="+Date.parse(new Date())+"' type='text/javascript'%3E%3C/script%3E"));
+
+document.write('</div>');
+
+};*/
